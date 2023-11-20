e7edc8d4ef0687443b59520f0662aa2423f48f17
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-11-20 18:00:59 +0100

    Rozwiązanie zadania

--- src/main/java/pl/javastart/task/BubbleSort.java
+++ src/main/java/pl/javastart/task/BubbleSort.java
@@ -4,6 +4,28 @@ public class BubbleSort {
 
     // ta metoda powinna sortować inputArray oraz zwracać informację o liczbie porównań, które były do tego potrzebne
     public int sort(int[] inputArray) {
-        return 0;
+        int n = inputArray.length;
+        int comparisons = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                comparisons++;
+
+                if (inputArray[j] > inputArray[j + 1]) {
+                    int temp = inputArray[j];
+                    inputArray[j] = inputArray[j + 1];
+                    inputArray[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+
+        return comparisons;
     }
 }
